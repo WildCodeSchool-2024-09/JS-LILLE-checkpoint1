5b787c8bcfd6787905fc3ffcd5b510cdc8d3161f
ALICIA <alicia.magro@gmail.com>
2024-10-21 12:29:36 +0200

    did my best with the dom js, I really don't like that

---
+++ site/js/portfolio.js
@@ -0,0 +1,28 @@
+// changeAvatar ((newAvatar) =>  {
+//     const firstAvatar = document.querySelector("#firstAvatar");
+//     firstAvatar.addEventListener (("click"), () => {
+//         alert ("ça marche ??");
+//         // const secondAvatar = document.createElement("img");
+//         // secondAvatar.classList.add("secondAvatar");
+//         // secondAvatar.src = "image/avatar.svg";
+//         // secondAvatar.alt = "another drawing representig Matt";
+//         // secondAvatar.appendChild(ajout);
+//     })
+// })
+
+
+// cardButton.classList.add("newButton");
+
+
+
+
+function event () { 
+const description = document.getElementsByClassName("description pink-bg");
+const firstButton = document.getElementsByClassName("firstButton");
+
+firstButton.addEventListener("click", () => {
+    firstButton.style.backgroundColor= "black";
+});
+}
+
+   
