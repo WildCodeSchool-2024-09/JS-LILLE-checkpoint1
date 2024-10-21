fb95be4798188ebff506eecd5af05e30fe6f78fa
Kyky Gaming <mesmacquetimothey@gmail.com>
2024-10-21 12:03:37 +0200

    algo 3 done

--- algo/3/findSeniors.js
+++ algo/3/findSeniors.js
@@ -5,28 +5,41 @@ lui-même contenant deux sous-tableaux :
 - Le second ne contient que des data analysts seniors
 (Étant donné qu'une personne est senior si elle a 5 ans d'expérience ou plus)
 
-const persons = [
-  { name: 'Mary', experience: 2, job: 'web dev' },
-  { name: 'Tony', experience: 6, job: 'data analyst' },
-  { name: 'John', experience: 2, job: 'data analyst' },
-  { name: 'Jane', experience: 6, job: 'web dev' },
-  { name: 'Maggie', experience: 2, job: 'web dev' },
-  { name: 'Leonardo', experience: 2, job: 'data analyst' },
-  { name: 'Carla', experience: 4, job: 'data analyst' },
-  { name: 'Mickael', experience: 7, job: 'web dev' },
-  { name: 'Penelope', experience: 7, job: 'web dev' },
-  { name: 'Homer', experience: 5, job: 'data analyst' },
-  { name: 'Leonardo', experience: 2, job: 'data analyst' },
-  { name: 'Carla', experience: 4, job: 'web dev' },
-  { name: 'Lisa', experience: 3, job: 'web dev' },
-  { name: 'Millie', experience: 5, job: 'data analyst' },
-  { name: 'Penelope', experience: 7, job: 'web dev' },
-];
 
 */
+const persons = [
+	{ name: "Mary", experience: 2, job: "web dev" },
+	{ name: "Tony", experience: 6, job: "data analyst" },
+	{ name: "John", experience: 2, job: "data analyst" },
+	{ name: "Jane", experience: 6, job: "web dev" },
+	{ name: "Maggie", experience: 2, job: "web dev" },
+	{ name: "Leonardo", experience: 2, job: "data analyst" },
+	{ name: "Carla", experience: 4, job: "data analyst" },
+	{ name: "Mickael", experience: 7, job: "web dev" },
+	{ name: "Penelope", experience: 7, job: "web dev" },
+	{ name: "Homer", experience: 5, job: "data analyst" },
+	{ name: "Leonardo", experience: 2, job: "data analyst" },
+	{ name: "Carla", experience: 4, job: "web dev" },
+	{ name: "Lisa", experience: 3, job: "web dev" },
+	{ name: "Millie", experience: 5, job: "data analyst" },
+	{ name: "Penelope", experience: 7, job: "web dev" },
+];
 
 function findSeniors(persons) {
-  // Your code here !
+	// Your code here !
+	const seniorWeb = [];
+	const seniorData = [];
+	const finaltab = [];
+	for (let i = 0; i < persons.length; i++) {
+		if (persons[i].experience >= 5 && persons[i].job === "web dev") {
+			seniorWeb.push(persons[i]);
+		} else if (persons[i].experience >= 5 && persons[i].job === "data analyst")
+			seniorData.push(persons[i]);
+	}
+	finaltab.push(seniorWeb);
+	finaltab.push(seniorData);
+	return finaltab;
 }
-
+// findSeniors(persons);
+// console.log(persons[0]);
 module.exports = findSeniors;
