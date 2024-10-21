75ab7cf1efcdc65f088538ab101e10c99a6040dc
Arnaud6216 <guevaer8@gmail.com>
2024-10-21 12:03:31 +0200

    Portfolio Javascript step 4 finished

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -7,9 +7,22 @@ avatarImg.addEventListener("click", function () {
 const button = document.getElementById("description-button");
 button.addEventListener("click", function () {
 	const userName = document.getElementById("firstname");
-	const bgColor = document.querySelector(".description.pink-bg");
+	const bgColor = document.getElementsByClassName("description pink-bg");
+	const textColor = document.getElementsByClassName("pink-text");
+
+	const chooseColor = prompt("Enter a color :");
 	const firstname = prompt("Enter your name :");
+
 	userName.textContent = firstname;
 	userName.style.color = "white";
-	bgColor.style.backgroundColor = "#750ff7";
+
+	// iteration of bgColor to apply style to each element of the HTML collection
+	for (let i = 0; i < bgColor.length; i++) {
+		bgColor[i].style.backgroundColor = chooseColor;
+	}
+
+	// iteration of textColor to apply style to each element of the HTML collection
+	for (let i = 0; i < textColor.length; i++) {
+		textColor[i].style.color = chooseColor;
+	}
 });
