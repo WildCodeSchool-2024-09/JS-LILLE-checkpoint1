59743ee4ccab54536f5ff525e30621f9cec800fb
Arnaud6216 <guevaer8@gmail.com>
2024-10-21 11:34:33 +0200

    Portfolio Javascript step 3 finished

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,10 +4,12 @@ avatarImg.addEventListener("click", function () {
 	avatarImg.src = "image/avatar.svg";
 });
 
-const userName = document.getElementById("firstname");
 const button = document.getElementById("description-button");
 button.addEventListener("click", function () {
-	let firstname = prompt("Enter your name :");
+	const userName = document.getElementById("firstname");
+	const bgColor = document.querySelector(".description.pink-bg");
+	const firstname = prompt("Enter your name :");
 	userName.textContent = firstname;
 	userName.style.color = "white";
+	bgColor.style.backgroundColor = "#750ff7";
 });
